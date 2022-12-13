62f5bb47aac330944fbcd6b8d451422d153cc468
Antonio Lanciotti <antonio.lanciotti@live.it>
2022-12-13 09:51:28 +0100

    fix return_json

--- backend-service/main.js
+++ backend-service/main.js
@@ -11,14 +11,13 @@ app.get("/", (req, res) => res.send("OK\n"));
 app.get("/path", (req, res) => {
     const required_path = req.query.path;
     const path_to_watch = path.join(static_path, required_path);
-    var return_json = [];
     if (!fs.existsSync(path_to_watch) || !fs.statSync(path_to_watch).isDirectory()){
         res.sendStatus(404);
         console.warn("NOT OK PATH", path_to_watch);
         return;
     }
     fs.readdir(path_to_watch, (err, files) => {
-        return_json = files.map(file => {
+        const return_json = files.map(file => {
             // if directory return name and extension FOLDER
             if (fs.statSync(path.join(path_to_watch, file)).isDirectory()) {
                 return {
